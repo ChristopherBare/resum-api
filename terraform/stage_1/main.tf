c2a2c41847229bbc5f1d397cdf065e39206ab666
Christopher Bare <christopherbare@outlook.com>
2023-10-04 10:46:17 -0400

    terraform apply

--- terraform/stage_1/main.tf
+++ terraform/stage_1/main.tf
@@ -9,7 +9,7 @@ data "github_repository" "repo" {
 
 # Step 4: Create an S3 bucket
 resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = "${var.bucket_short_name}-${var.repository_name}-${var.branch_name}"
+  bucket = "${var.bucket_short_name}-${var.github_repo}-${var.branch_name}"
 }
 
 
