82ebff402715b4e90f0ffb3f713df19a0476eb75
Christopher Bare <christopherbare@outlook.com>
2023-10-06 11:35:06 -0400

    deploy

--- terraform/stage_1/backend.tf
+++ terraform/stage_1/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-backend-state-bucket-lambda-resum-api" # Replace with your S3 bucket name
-    key            = "terraform.tfstate"         # Replace with your state file name
-    region         = "us-east-1"                 # Replace with your desired AWS region
-#    encrypt        = true                        # Optional: enable encryption
-#    dynamodb_table = "my-lock-table"             # Optional: use a DynamoDB table for locking
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = "terraform-backend-state-bucket-lambda-resum-api" # Replace with your S3 bucket name
+#    key            = "terraform.tfstate"         # Replace with your state file name
+#    region         = "us-east-1"                 # Replace with your desired AWS region
+##    encrypt        = true                        # Optional: enable encryption
+##    dynamodb_table = "my-lock-table"             # Optional: use a DynamoDB table for locking
+#  }
+#}
