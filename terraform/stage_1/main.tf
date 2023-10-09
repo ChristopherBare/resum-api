4651811a387ff9856dcb744ed4d3e15e1b42cd34
Christopher Bare <christopherbare@outlook.com>
2023-10-09 14:44:01 -0400

    deploy

--- terraform/stage_1/main.tf
+++ terraform/stage_1/main.tf
@@ -12,17 +12,13 @@ data "github_repository" "repo" {
   full_name = var.github_repo_full.name
 }
 
-resource "null_resource" "set_create_bucket" {
-  triggers = {
-    create_bucket = var.create_bucket
-  }
-
-  provisioner "local-exec" {
-    command = "echo 'create_bucket = ${var.create_bucket}' > create_bucket.auto.tfvars"
-  }
+locals {
+  create_bucket = data.terraform_remote_state.stage_1.outputs.bucket_exists ? false : true
 }
+
+
 resource "aws_s3_bucket" "lambda_bucket" {
-  count = data.terraform_remote_state.stage_1.outputs.bucket_exists
+  count = var.create_bucket ? 1 : 0
   bucket = "${var.bucket_short_name}-${var.github_repo}-${var.branch_name}"
 }
 
@@ -30,7 +26,7 @@ resource "aws_s3_bucket" "lambda_bucket" {
 variable "create_bucket" {
   description = "Create the S3 bucket if it doesn't exist"
   type        = bool
-  default     = data.terraform_remote_state.stage_1.outputs.bucket_exists ? false : true
+  default     = false
 }
 
 
