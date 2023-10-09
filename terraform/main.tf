e02fd04b73104cf4fc4e3e7495bd588945fc6ab5
Christopher Bare <christopherbare@outlook.com>
2023-10-09 17:30:23 -0400

    refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,10 @@ data "github_repository" "repo" {
   full_name = var.github_repo_full.name
 }
 
+locals {
+  lambda_package_checksum = filebase64(var.lambda_package_path)
+}
+
 resource "random_id" "unique_id" {
   byte_length = 8 # You can adjust the length as needed
 }
@@ -11,8 +15,14 @@ resource "aws_lambda_function" "resum_api_lambda" {
   role          = data.aws_iam_role.lambda_exec_role.arn
   handler       = "main"
   runtime       = "go1.x"
+  source_code_hash = local.lambda_package_checksum
+  filename = var.lambda_package_path
+}
 
-  filename = "../lambda.zip/lambda.zip"
+variable "lambda_package_path" {
+  description = "Path to the Lambda deployment package"
+  type        = string
+  default     = "../lambda.zip/lambda.zip"
 }
 
 data "aws_iam_role" "lambda_exec_role" {
