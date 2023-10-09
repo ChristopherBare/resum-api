c4159dd919334b04d58686bf3a663a8682411977
Christopher Bare <christopherbare@outlook.com>
2023-10-09 14:26:49 -0400

    deploy

--- terraform/stage_1/main.tf
+++ terraform/stage_1/main.tf
@@ -1,26 +1,20 @@
+data "terraform_remote_state" "stage_1" {
+  backend = "s3"
+
+  config = {
+    bucket = "terraform-backend-state-bucket-lambda-resum-api"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 data "github_repository" "repo" {
   full_name = var.github_repo_full.name
 }
 
-resource "null_resource" "set_create_bucket" {
-  triggers = {
-    create_bucket = var.create_bucket
-  }
-
-  provisioner "local-exec" {
-    command = "echo 'create_bucket = ${var.create_bucket}' > create_bucket.auto.tfvars"
-  }
-}
 resource "aws_s3_bucket" "lambda_bucket" {
-  count  = var.create_bucket ? 1 : 0
+  count = data.terraform_remote_state.stage_1.outputs.bucket_exists
   bucket = "${var.bucket_short_name}-${var.github_repo}-${var.branch_name}"
-}
-
-# Set the `create_bucket` variable based on the existence of the S3 bucket
-variable "create_bucket" {
-  description = "Create the S3 bucket if it doesn't exist"
-  type        = bool
-  default     = true
 }
 
 
--- terraform/stage_1/outputs.tf
+++ terraform/stage_1/outputs.tf
@@ -1,3 +1,7 @@
 output "bucket_name" {
   value = aws_s3_bucket.lambda_bucket[0].bucket
 }
+
+output "bucket_exists" {
+  value = aws_s3_bucket.lambda_bucket ? true : false
+}
