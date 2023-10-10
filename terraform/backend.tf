9d83a135d9ae56a84e522cb7a36abb43e5191099
Christopher Bare <christopherbare@outlook.com>
2023-10-10 14:51:06 -0400

    refactor

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-backend-state-bucket-lambda-resum-api"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "terraform-lock-table-resum-api"
+    bucket         = "terraform-backend-state-bucket-lambda-resum-api"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
   }
 }
