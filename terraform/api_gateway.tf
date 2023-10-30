a7b3d2e0ecd811da11bee7b1547f2fcc97807f31
Christopher Bare <christopherbare@outlook.com>
2023-10-30 13:19:18 -0400

    tweaking iam roles in api

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -3,7 +3,7 @@ resource "aws_api_gateway_rest_api" "resum_api" {
   description = "A resume, as an API"
 }
 
-resource "aws_api_gateway_resource" "root" {
+resource "aws_api_gateway_resource" "api_resource" {
   parent_id   = aws_api_gateway_rest_api.resum_api.root_resource_id
   path_part   = "resume"
   rest_api_id = aws_api_gateway_rest_api.resum_api.id
@@ -11,14 +11,14 @@ resource "aws_api_gateway_resource" "root" {
 
 resource "aws_api_gateway_method" "api_method" {
   rest_api_id   = aws_api_gateway_rest_api.resum_api.id
-  resource_id   = aws_api_gateway_resource.root.id
+  resource_id   = aws_api_gateway_resource.api_resource.id
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "api_integration" {
   rest_api_id = aws_api_gateway_rest_api.resum_api.id
-  resource_id = aws_api_gateway_resource.root.id
+  resource_id = aws_api_gateway_resource.api_resource.id
   http_method = aws_api_gateway_method.api_method.http_method
 
   integration_http_method = "POST"
