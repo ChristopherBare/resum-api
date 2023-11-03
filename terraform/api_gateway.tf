ae7cac0bf2b2261934fad697bdf456d1468d83fb
Christopher Bare <christopherbare@outlook.com>
2023-11-02 22:11:51 -0400

    added CORS stuff to api gateway

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -48,10 +48,11 @@ resource "aws_api_gateway_method" "options" {
 }
 
 resource "aws_api_gateway_integration" "options" {
-  rest_api_id = aws_api_gateway_rest_api.resum_api.id
-  resource_id = aws_api_gateway_resource.api_resource.id
-  http_method = aws_api_gateway_method.options.http_method
-  type = "MOCK"
+  rest_api_id             = aws_api_gateway_rest_api.resum_api.id
+  resource_id             = aws_api_gateway_resource.api_resource.id
+  http_method             = aws_api_gateway_method.options.http_method
+  integration_http_method = "OPTIONS"
+  type                    = "MOCK"
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
   }
