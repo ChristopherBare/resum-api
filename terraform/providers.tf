b8254d0eb2b756a4072c4490400a0e33e36a6d8f
Christopher Bare <christopherbare@outlook.com>
2023-11-09 13:00:12 -0500

    CORS

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,6 +11,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "GITHUB_TOKEN" {}
+
 provider "github" {
-  token = var.GITHUB_TOKEN
+  token = var.GITHUB_TOKEN != "" ? var.GITHUB_TOKEN : env("GITHUB_TOKEN")
 }
