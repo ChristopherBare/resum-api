37264df1f4000550e090fb79d5f8d2ae25cb2df1
Christopher Bare <christopherbare@outlook.com>
2023-11-09 12:26:50 -0500

    CORS

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,13 @@
+terraform{
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
+provider "github" {}
