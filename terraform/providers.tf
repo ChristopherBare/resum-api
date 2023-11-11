4a246fe426c23df6f32935ab53c1a548ae90467d
Christopher Bare <christopherbare@outlook.com>
2023-11-11 14:40:41 -0500

    CORS

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 5.0"
     }
   }
 }
@@ -11,21 +10,4 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "GITHUB_TOKEN" {
-  description = "Secret value for token"
-}
-
-locals {
-  github_token_secret_value = "%s"
-}
-
-resource "github_actions_environment_secret" "GITHUB_TOKEN" {
-  repository      = data.github_repository.repo.name
-  environment     = "prod"
-  secret_name     = "MY_GITHUB_TOKEN"
-  plaintext_value = local.github_token_secret_value
-}
-
-provider "github" {
-  token = local.github_token_secret_value
-}
+provider "github" {}
