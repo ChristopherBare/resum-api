da50f1c32260d56213430d96e3a389239ed3494d
Christopher Bare <christopherbare@outlook.com>
2025-09-05 15:20:25 -0400

    Feature/terraform update 3 (#21)
    * removed un-needed log statement
    
    * updated versions in actions yml
    
    * removed deprecated lines
    
    * fixed invoke_url error
    
    * updated outputs.tf

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -85,3 +85,10 @@ resource "aws_api_gateway_deployment" "lambda" {
   depends_on  = [aws_api_gateway_integration.api_integration, aws_api_gateway_integration.options_integration]
   rest_api_id = aws_api_gateway_rest_api.resum_api.id
 }
+
+# Create a stage to make the deployment accessible via a URL
+resource "aws_api_gateway_stage" "prod" {
+  rest_api_id   = aws_api_gateway_rest_api.resum_api.id
+  deployment_id = aws_api_gateway_deployment.lambda.id
+  stage_name    = "prod"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,7 @@ output "lambda_function_arn" {
 }
 
 output "api_gateway_invoke_url" {
-  value = "https://${aws_api_gateway_rest_api.resum_api.id}.execute-api.${data.aws_region.current.name}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}"
+  value = "https://${aws_api_gateway_rest_api.resum_api.id}.execute-api.${data.aws_region.current.id}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}"
 }
 
 output "lambda_function_name" {
